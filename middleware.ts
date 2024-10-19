09107980edfe73d050d734b89fb5732699f8780a
TuanKiet091202 <tuankieta4hbt@gmail.com>
2024-10-20 02:39:18 +0700

    sec

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
+// Mở quyền truy cập cho các route công khai
 export default authMiddleware({
-  publicRoutes: ["/api/:path*", "/ws/ws"]
+  publicRoutes: ["/checkout", "/cart", "/api/:path*", "/ws/ws"], // Thêm checkout và cart
 });
 
+// Cấu hình matcher để middleware áp dụng chính xác
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
